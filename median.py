f9edb74e976a42598273a21e6090494cd4e4e769
William Lingard <William.Lingard@kcl.ac.uk>
2022-10-02 21:32:01 +0100

    Comments

--- median.py
+++ median.py
@@ -7,8 +7,10 @@ while True:
         numbers = [float(value) for value in input().split(",")]
         numbers.sort()
         if len(numbers) % 2 != 0:
+            # Odd length calculation
             print(numbers[int((len(numbers) -1) /2)])
         else:
+            # Even length calculation
             print((numbers[int(len(numbers)/2)]+numbers[int((len(numbers)/2)-1)])/2)
     except ValueError:
         print("Some input could not be converted to a number!")
